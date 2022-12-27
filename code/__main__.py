ab303e8d35e53212d09f8c6f6c8bc842d7a4d655
Zara Siddique <zara@siddique.eu>
2022-12-27 16:21:55 +0000

    Added preprocessing steps for data

--- code/__main__.py
+++ code/__main__.py
@@ -1,8 +1,23 @@
+import load_data
 import clean_data
+import train_model
 import load_model
+import ibd
 
-def main():
-    client = load_model.load_news_model()
+def preprocessing():
+    print("Starting preprocessing")
+    data = load_data.main()
+    print(data[0])
+    cleandata = clean_data.main(data)
+    print(cleandata[0])
+    model = train_model.main(cleandata)
+    return model
+
+def evaluation():
+    model = load_model.main()
+    print(len(model.wv))
+    return True
 
 if __name__ == "__main__": 
-	main()
+    #model = preprocessing()
+    evaluation()
